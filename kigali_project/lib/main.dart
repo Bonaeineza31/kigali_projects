cf360af740437a9a29613db0f5f36f8d5907f17c
Bonaeineza31 <b.ineza@alustudent.com>
2026-03-02 23:17:33 +0200

    moved the api key to the .env file

--- kigali_project/lib/main.dart
+++ kigali_project/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'providers/auth_provider.dart';
 import 'providers/listing_provider.dart';
 import 'screens/login_screen.dart';
@@ -9,6 +10,7 @@ import 'screens/verification_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await Firebase.initializeApp();
   runApp(
     MultiProvider(
